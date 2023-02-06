1acbf99f8421a54a39f8dfee35fbe764b2278553
Beaguhl <ieez@live.se>
2023-02-06 13:04:19 +0100

    Implemented Startpage

--- Wishes/frontend/src/App.svelte
+++ Wishes/frontend/src/App.svelte
@@ -1,45 +1,55 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+	import {Router, Link, Route} from 'svelte-routing'
+	import StartPage from "./lib/StartPage.svelte"
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+<Router>
+	<div class="mainDiv">
+			<h1 class="wishes">
+				<Link class="Links" to="/">Wishes</Link>
+			</h1>
+			<nav class="navBar">
+				<Link class="Links" to="/FindWishes">Find Wishes</Link>
+				<Link class="Links" to="/FindUsers">Find Users</Link>
+				<Link class="Links" to="/Login">PlaceHolder</Link>
+			</nav>
+	</div>
+	<div>
+		<StartPage></StartPage>
+	</div>
+</Router>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	:global(body){
+		margin: 0;
+		padding: 0;
+		height: 100vh;
+		width: 100%;
+	}
+
+	.mainDiv{
+		display: flex;
+		width: 80vw;
+		background-color: gray;
+	}
+
+
+	.navBar{
+		margin-top: 4%;
+		display: flex;
+		justify-content: space-around;
+		align-items: flex-start;
+		width: 100%;
+	}
+
+	.wishes{
+		display: flex;
+		width: fit-content;
+		float: left;
+		margin-left: 5%;
+	}
+
 </style>
