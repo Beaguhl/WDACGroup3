feb45a96bcc226945377632e5ca8b719b318cc56
ellencarlsson <98748113+ellencarlsson@users.noreply.github.com>
2023-02-10 00:11:37 +0100

    created WishList in specific user page

--- Wishes/frontend/src/lib/SpecificUser.svelte
+++ Wishes/frontend/src/lib/SpecificUser.svelte
@@ -1,8 +1,11 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.8.0/css/bulma.min.css">
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css
+">
 <script>
+    import { text } from "svelte/internal";
+
    
 
+    
    class WishList {
           constructor(itemName, purchased) {
               this.itemName = itemName;
@@ -21,7 +24,7 @@
 
     // ------------------- following button -------------
     var follow = false
-    document.getElementById("follow").addEventListener("click", ToggleFollow)
+    /*document.getElementById("follow").addEventListener("click", ToggleFollow)
     
     function ToggleFollow() {
       
@@ -35,112 +38,196 @@
         follow = true
       }
 
-    }
+    }*/
+    //<img id="profilePic" src="https://preview.redd.it/v0caqchbtn741.jpg?auto=webp&s=c5d05662a039c031f50032e22a7c77dfcf1bfddc" alt="">
+  
+    
+  
   })
 
 </script>
 
 <!------------ HTML code ----------->
 
-  <div class="box">  
-    <div class="columns">
-    <div class="column">
+<body>
 
-      <figure class="image" >
-        <img id="profilePic" src="https://preview.redd.it/v0caqchbtn741.jpg?auto=webp&s=c5d05662a039c031f50032e22a7c77dfcf1bfddc" alt="">
-      </figure>
-      <h5 class="subtitle is-5" id="userName">User Name</h5>
-      <div>
-        <button id="follow"> </button>
+     <div id="wishListObject">
+      <h1 class="title">
+        User Name's Wish List
+      </h1>
+
+      <div class="wishList">
+        {#each arrayOfWishes as wish}
+        <div class="item">
+
+          <div class="item-btn">
+            <!-- dessa if satser är inte de snygaste men får duga för frontend sålänge -->
+            {#if wish.purchased}
+              <i class="fa-regular fa-square-check"></i>
+            {:else}
+              <i class="fa-regular fa-square"></i>
+            {/if}
+            
+          </div>
+          
+          <p id="itemTitle">{wish.itemName}</p>
+
+          {#if wish.purchased}
+          <div class="item-btn">
+            
+          
+          <i id="closed" class="fa-solid fa-chevron-down"></i>
+          <!--<i class="fa-solid fa-xmark"></i>-->
+          
+            
+            
+          </div>
+          <span></span>
+          <div id="increasedItem">
+            <div id="buyer">Bought by: {wish.purchased}</div>
+          </div>
+          {/if}
+
+        </div>
+        
+
+
+        <!-- increased item view -->
+       
+        {/each}
+        
+
       </div>
     </div>
-    <div class="column is-one-fifth">
-    </div>
-    <div class="column">
-
-      <h4 class="subtitle is-4">"user name's" WishList</h4>
-
-      <ul id="wishList">
-        {#each arrayOfWishes as item}
-          <li id="item">
-            <div class="block">
-              <div class="columns is-mobile">
-                <!------------ item title --------->
-                <div class="column is-four-fifths">
-                  <p class="has-text-left" id="itemTitle">{item.itemName}</p>
-                </div>
-                <!------------ + hoverable --------->
-                <div class="column">
-                    <div class="dropdown is-hoverable is-right">
-                      <div class="dropdown-trigger">
-                        <p aria-haspopup="true" aria-controls="dropdown-menu4">
-                          <!------------ "+" tecknet ska ersättas med ✔️ --------->
-                          <!------------ ifall item är purchased --------->
-                          <span id="itemDrop" class="has-text-right">✚</span>
-                        </p>
-                      </div>
-                      <div class="dropdown-menu" id="dropdown-menu4" role="menu">
-                        <!------------ drowp down content --------->
-                        <!------ här ska javascript avgöra vad som ska stå i content ---->
-                        <!------ är item purchased ska det stå vem som gjort det -------->
-                        <!------ annar ska det stå att man kan köpa den med en check box --------->
-                        <div class="dropdown-content">
-                          <div class="dropdown-item" id="dropDownContent">                                
-                              <label class="checkbox">
-                                <input type="checkbox"> I've bought this item</label>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                </div>
-              </div>
-            </div>
-          </li>
-        {/each}
-      </ul>
-    </div>
-  </div>
-</div>
+</body>
   
 
 <style>
 
-  #follow {
-    margin-bottom: 20px;
-    margin-top: 20px;
+  .title{
+    text-align: center;
   }
 
-  #username {
-    margin-bottom: 70px;
+
+  #increasedItem {
+    padding-bottom: 15px;
+    background-color: brown;
+    padding-top: 1%;
+
+    
   }
 
+  #itemTitle {
+    text-align: center;
+  }
+
+  #buyer {
+    text-align: start;
+  }
+
+
+  body {
+    background-color: aqua;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    width: 100vw;
+  }
+
+  .wishList {
+    width: 450px;
+    padding: 18px;
+    box-shadow: 0 10px 40px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    background-color: blanchedalmond;
+
+  }
+
+  .title {
+    font-size: 30px;
+    font-weight: 600;
+    letter-spacing: 1px;
+    margin: 30px 0;
+    text-align: center;
+  }
+
+  .wishList{
+    width: 100%;
+    margin-top: 30px;
+  }
+  
+  #itemTitle{
+    text-align: start;
+    font-size: 16px;
+    font-weight: 500;
+    letter-spacing: 1px;
+  }
+
+  .item{
+    border-radius: 5px;
+    box-shadow: 1 10px 30px rgba(0, 0, 0, 0.1);
+    display: flex;
+    align-items: center;
+    display: grid;
+    grid-template-columns: 1fr 4fr 1fr;
+    background-color: brown;
+    margin-top: 15px;
+  }
+
+  .item-btn{
+    justify-content: space-between;
+    align-items: center;
+    gap: 10px;
+    cursor: pointer;
+  }
+
+  
+
+  .item-btn i {
+    font-size: 18px;
+  }
+
+  #grid{
+    display: grid;
+    grid-template-columns: 2fr 2fr;
+    grid-template-rows: 2fr 2fr;
+  }
+
+  #userNameAndFollow{
+    text-align: center;
+    max-width: 100%;
+    max-height: 100%;
+    display: block
+  }
+
+  #profilePic{
+    text-align: center;
+    margin-top: 4%;
+    margin-left: 4%;
+    max-width: 100%;
+    max-height: 100%;
+    display: block
+  }
+
+  #wishListObject{
+    text-align: center;
+    margin-top: 40px;
+  }
+
+  #wishList{
+    text-align: center;
+    margin-left: 8%;
+    margin-right: 8%;
+    margin-top: 3%;
+    max-width: 100%;
+    max-height: 100%;
+    display: block
+  }
+ 
   #profilePic {
     border-radius: 10%;
   }
 
-  #item {
-    background-color: rgb(231, 231, 231);
-    margin-bottom: 20px;
-  
-  }
-
-  #itemTitle {
-    margin-left: 20px;
-  }
-
-  #itemDrop {
-    margin-right: 10px;
-  }
-
-  #wishList {
-    margin-top: 40px;
-  }
-
-  #userName {
-    margin-top: 20px;
-  }
-
 </style>
-
-
-
