14e07a330ae84f4ee161020486ae633c43cc4872
Beaguhl <ieez@live.se>
2023-03-24 12:30:11 +0100

    Fixed more stuff for db

--- application/database/init.sql
+++ application/database/init.sql
@@ -13,6 +13,18 @@ INSERT INTO Users (username, password, admin)
 VALUES ("ellen", "$2b$12$WsKB6biwEx.IE07bKf.XHOBtWDvSCEShh01ymS/.3DsZ9/DmuIyXq", TRUE);
 INSERT INTO Users (username, password, admin)
 VALUES ("nisse", "$2b$12$h1W/m.Ut5L99..OaZNaYcelKI/6jT.VbAT2CKkI2arM.jG57D9DNK", FALSE);
+INSERT INTO Users (username, password, admin)
+VALUES ("jocke1", "$2b$12$SxN/g01Ktw4BVmaPGsE0q.94UoRtYI/QvPlwkyheUgkHnubxI90vm", TRUE);
+INSERT INTO Users (username, password, admin)
+VALUES ("ellen1", "$2b$12$WsKB6biwEx.IE07bKf.XHOBtWDvSCEShh01ymS/.3DsZ9/DmuIyXq", TRUE);
+INSERT INTO Users (username, password, admin)
+VALUES ("nisse1", "$2b$12$h1W/m.Ut5L99..OaZNaYcelKI/6jT.VbAT2CKkI2arM.jG57D9DNK", FALSE);
+INSERT INTO Users (username, password, admin)
+VALUES ("jocke2", "$2b$12$SxN/g01Ktw4BVmaPGsE0q.94UoRtYI/QvPlwkyheUgkHnubxI90vm", TRUE);
+INSERT INTO Users (username, password, admin)
+VALUES ("ellen2", "$2b$12$WsKB6biwEx.IE07bKf.XHOBtWDvSCEShh01ymS/.3DsZ9/DmuIyXq", TRUE);
+INSERT INTO Users (username, password, admin)
+VALUES ("nisse2", "$2b$12$h1W/m.Ut5L99..OaZNaYcelKI/6jT.VbAT2CKkI2arM.jG57D9DNK", FALSE);
 
 
 CREATE TABLE IF NOT EXISTS Follow (
@@ -58,6 +70,14 @@ INSERT INTO Products (productName, description) VALUES ("Jeans", "Cropped jeans"
 INSERT INTO Products (productName, description) VALUES ("Coffecup", "Pink coffecup");
 INSERT INTO Products (productName, description) VALUES ("Phone case", "Protects your phone");
 INSERT INTO Products (productName, description) VALUES ("Cables", "Connect things");
+INSERT INTO Products (productName, description) VALUES ("Cole", "Black drink");
+INSERT INTO Products (productName, description) VALUES ("Eraser", "Remove your mistakes");
+INSERT INTO Products (productName, description) VALUES ("Murder weapon", "Soft and cuddly");
+INSERT INTO Products (productName, description) VALUES ("Drink", "Sangria");
+INSERT INTO Products (productName, description) VALUES ("My little pony", "It is emo");
+INSERT INTO Products (productName, description) VALUES ("Troll", "Big hair");
+INSERT INTO Products (productName, description) VALUES ("Mufasa", "He is dead");
+INSERT INTO Products (productName, description) VALUES ("Movie", "A dvd");
 
 
 CREATE TABLE IF NOT EXISTS WishListProduct (
@@ -73,11 +93,19 @@ CREATE TABLE IF NOT EXISTS WishListProduct (
 INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (1, 1, TRUE, 2);
 INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (2, 2, FALSE, NULL);
 INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (6, 3, TRUE, 1);
-INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (3, 2, FALSE, NULL);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (2, 2, FALSE, NULL);
 INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (1, 3, FALSE, NULL);
-INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (3, 2, TRUE, 3);
-INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (4, 3, FALSE, NULL);
-INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (3, 2, FALSE, NULL);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (4, 4, TRUE, 3);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (14, 5, FALSE, NULL);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (15, 6, FALSE, NULL);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (9, 1, TRUE, 2);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (3, 7, FALSE, NULL);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (13, 8, TRUE, 1);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (15, 5, FALSE, NULL);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (7, 3, FALSE, NULL);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (3, 6, TRUE, 3);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (11, 2, FALSE, NULL);
+INSERT INTO WishListProduct (productID, wishListID, purchased, userPurchased) VALUES (12, 5, FALSE, NULL);
 
 
 
