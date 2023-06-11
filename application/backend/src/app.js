3a2655448d0b3378dc9374851bb15521b564050f
Beaguhl <ieez@live.se>
2023-06-11 15:30:07 +0200

    app.js and added file
    Added a context file for pool and other context heavy varables

--- application/backend/src/app.js
+++ application/backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { createPool } = require("mariadb");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const pool = require('./context.js')
 
 const followersRouter = require("./routers/followers-router");
 const followingsRouter = require("./routers/followings-router");
@@ -13,13 +13,6 @@ const wishListproduct = require("./routers/wishlist-product-router");
 
 const ACCESS_TOKEN_SECRET = "PN#/(dh6-.E.x-'P2";
 
-const pool = createPool({
-	host: "db",
-	port: 3306,
-	user: "root",
-	password: "abc123",
-	database: "abc",
-});
 
 pool.on("error", function (error) {
 	console.log("Error from pool", error);
@@ -39,89 +32,90 @@ app.use(function (request, response, next) {
 	next();
 });
 
-app.get("/", function (request, response) {
-	response.send("It works");
-});
-
 //----------------------- tokens ----------------------
 app.post("/tokens", async function (request, response) {
 	const grantType = request.body.grant_type;
 	const username = request.body.username;
 	const password = request.body.password;
 
-	const connection = await pool.getConnection();
+	if (grantType != "password") {
+		response.status(400).json({ error: "unsupported_grant_type" }).end();
+		return;
+	}
 
 	try {
-		if (grantType != "password") {
-			response.status(400).json({ error: "unsupported_grant_type" }).end();
-			return;
-		}
+		const connection = await pool.getConnection()
+		try {
 
-		if (username == "" || password == "") {
-			response.status(400).json({ error: "invalid_request" }).end();
-			return;
-		}
+			if (username == "" || password == "") {
+				response.status(400).json({ error: "invalid_request" }).end();
+				return;
+			}
 
-		const compareUserQuery = "SELECT * FROM Users WHERE username = ?";
-		const compareUserValue = [username];
+			const getUserQuery = "SELECT * FROM Users WHERE username = ?";
+			const getUserValue = [username];
 
-		const result = await connection.query(compareUserQuery, compareUserValue);
+			const accounts = await connection.query(getUserQuery, getUserValue);
 
-		if (result.length != 0) {
-			bcrypt.compare(password, result[0].password, (err, res) => {
-				if (err) {
-					throw err;
-				}
-				if (res === true) {
-					const payload = {
-						sub: `${result[0].userID}`,
-					};
+			if (accounts.length == 0) {
+				response.status(400).json({ error: "invalid_grant" });
+				return;
+			} else {
+				bcrypt.compare(password, accounts[0].password, (err, res) => {
+					if (err) {
+						throw err;
+					}
+					if (res !== true) {
+						response.status(403).end();
+					} else {
+						const payload = {
+							sub: `${accounts[0].userID}`,
+						};
 
-					jwt.sign(payload, ACCESS_TOKEN_SECRET, function (error, accesToken) {
-						if (error) {
-							response.status(500).end();
-						} else {
-							const payloadIDToken = {
-								sub: `${result[0].userID}`,
-								iss: `http://localhost:8080`,
-								aud: `wishes.com`,
-								exp: Math.floor(Date.now() / 1000) + 600,
-							};
+						jwt.sign(payload, ACCESS_TOKEN_SECRET, function (error, accessToken) {
+							if (error) {
+								response.status(500).end();
+							} else {
+								const payloadIDToken = {
+									sub: `${accounts[0].userID}`,
+									iss: `http://localhost:8080`,
+									aud: `wishes.com`,
+									exp: Math.floor(Date.now() / 1000) + 600,
+								};
 
-							jwt.sign(
-								payloadIDToken,
-								ACCESS_TOKEN_SECRET,
-								function (error, id_token) {
-									if (error) {
-										response.status(500).end();
-									} else {
-										response.status(200).json({
-											access_token: accesToken,
-											id_token: id_token,
-											type: "bearer",
-											userID: result[0].userID,
-											admin: result[0].admin,
-										});
+								jwt.sign(
+									payloadIDToken,
+									ACCESS_TOKEN_SECRET,
+									function (error, id_token) {
+										if (error) {
+											response.status(500).end();
+										} else {
+											response.status(200).json({
+												access_token: accessToken,
+												id_token: id_token,
+												type: "bearer",
+												userID: accounts[0].userID,
+												admin: accounts[0].admin,
+											});
+										}
 									}
-								}
-							);
-						}
-					});
-				} else {
-					response.status(403).end();
-				}
-			});
-		} else {
-			response.status(400).json({ error: "invalid_grant" });
-			return;
+								);
+							}
+						});
+					}
+				});
+			}
+		} catch (error) {
+			console.log(error)
+			response.status(500).end();
+		} finally {
+			if (connection) {
+				connection.release();
+			}
 		}
 	} catch (error) {
 		console.log(error)
-		response.status(500).end();
-	} finally {
-		if (connection) {
-			connection.release();
-		}
+		response.status(500)
 	}
 });
 
